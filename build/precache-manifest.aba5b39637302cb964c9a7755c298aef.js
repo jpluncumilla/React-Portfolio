a11687c106c61abd71c237e22fade42e9e1a684b
Juan Pablo <jpluncumilla@gmail.com>
2020-07-10 18:20:53 -0400

    Responsiveness test

---
+++ build/precache-manifest.aba5b39637302cb964c9a7755c298aef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2acd1bfeb1c7f340a1328b41a5a22b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2caa39d6585bce10e7",
+    "url": "/static/css/main.7c0011d1.chunk.css"
+  },
+  {
+    "revision": "5eeb64b36e720b9bd3de",
+    "url": "/static/js/2.ff571338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff571338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2caa39d6585bce10e7",
+    "url": "/static/js/main.2cd80dc6.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "a1a4e2c288178975c86303a7d8ea7038",
+    "url": "/static/media/project1.a1a4e2c2.png"
+  },
+  {
+    "revision": "2bdd9eab31d5f20bcc2d62a36b454de8",
+    "url": "/static/media/project2.2bdd9eab.png"
+  },
+  {
+    "revision": "e15ba1bfc8fa682b0c1f72d38e2c170f",
+    "url": "/static/media/project3.e15ba1bf.png"
+  },
+  {
+    "revision": "f48e72119996b80a587d5b2a3a82c92b",
+    "url": "/static/media/project4.f48e7211.png"
+  }
+]);
